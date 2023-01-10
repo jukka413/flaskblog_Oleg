21ba2ee16792513f18b40158a8bfc479fc86ea58
jukka413 <jukka413@yandex.ru>
2023-01-10 18:59:23 +0500

    add edit_post, admin_panel - lesson 10

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Configuration
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 
 app = Flask(__name__)
@@ -10,3 +12,8 @@ app.config.from_object(Configuration)
 db = SQLAlchemy(app)
 from models import db
 migrate = Migrate(app, db)
+
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/for_flask'
+    SECRET_KEY = 'Very Secret'
